b34f8c9c10c03145b0be29e7599bb05e8643ad08
manish kumar tiwari <matul31517@gmail.com>
2023-01-09 23:52:03 +0530

    adding the crud operation in speaker and session class

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/controllers/SessionController.java
@@ -2,6 +2,7 @@ package com.pluralsight.conferencedemo.controllers;
 
 import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,8 +29,22 @@ public class SessionController {
       public  Session create(@RequestBody final  Session session){
           return sessionRepository.saveAndFlush(session);
       }
-
-      public void delete (){
-          
+      @RequestMapping(value = {"id"}, method = RequestMethod.DELETE)
+      public void delete (@PathVariable Long id){
+            //Also need to check for children records before deleting
+            sessionRepository.deleteById(id);
       }
+
+      @RequestMapping(value = {"id"}, method =  RequestMethod.PUT)
+      public  Session update (@PathVariable Long id , @RequestBody final Session session){
+            Session existingSession = sessionRepository.getOne(id);
+            //TODO: add  validation that all attribute are passed in otherwise return a 400 bad payload
+            BeanUtils.copyProperties(session,existingSession,"session_id");
+            return sessionRepository.saveAndFlush(existingSession);
+      }
+
+
+
+
+
 }
