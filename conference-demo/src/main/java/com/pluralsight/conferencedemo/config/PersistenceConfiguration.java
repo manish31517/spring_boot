cc6a7518371eeecf0f870ded8f49b4510c12f5f5
manish kumar tiwari <matul31517@gmail.com>
2023-02-26 22:09:42 +0530

    adding the railway database feature

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -1,21 +1,21 @@
-package com.pluralsight.conferencedemo.config;
-;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.util.unit.DataSize;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder  = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/my_database");
-        builder.username("postgres");
-        builder.password("Atul@1234");
-        System.out.println("My custom datasource has been initiazlied");
-        return builder.build();
-    }
-}
+//package com.pluralsight.conferencedemo.config;
+//;
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.util.unit.DataSize;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class PersistenceConfiguration {
+//    @Bean
+//    public DataSource dataSource(){
+////        DataSourceBuilder builder  = DataSourceBuilder.create();
+////        builder.url("jdbc:postgresql://localhost:5432/my_database");
+////        builder.username("postgres");
+////        builder.password("Atul@1234");
+////        System.out.println("My custom datasource has been initiazlied");
+////        return builder.build();
+//    }
+//}
